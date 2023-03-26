95d9a596ee31e85d2e77518d24aa9859690ffe7e
algopankaj <pankaj21905@gmail.com>
2023-03-26 21:09:43 +0530

    File added

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnOpenModal);
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    // console.log('Button-clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  //   console.log('A key is pressed !');
+  //   console.log(e.key);
+  if (e.key === 'Escape') {
+    console.log('Esc key is presses !');
+  }
+});
